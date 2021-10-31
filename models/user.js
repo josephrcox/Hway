d170274d2dbc4afb3ec395087839865fdafa65d2
mapleweekend <73198556+mapleweekend@users.noreply.github.com>
2021-10-30 21:37:25 -0700

    login/creation/jwt

--- models/user.js
+++ models/user.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema(
     {
         name: { type:String, unique:true },
-        password: { type:String }
+        password: { type:String },
+        date: { type: Date, default: Date.now },
+        post_count: { type: Number, default: 0},
+        comment_count: { type: Number, default: 0},
+        total_votes: { type: Number, default: 0},
+        account_active: { type: Boolean, default: true}
     }, 
     { collection: 'users'}
 )
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bcrypt = require('bcryptjs')
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 
 const users = []
 
@@ -14,6 +16,7 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATEBASE_URL, {
@@ -28,7 +31,7 @@ connection.once("open", function() {
 const User = require('./models/user')
 const jwt = require('jsonwebtoken')
 
-const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+const JWT_SECRET = process.env.JWT_SECRET
 
 const bp = require('body-parser')
 app.use(bp.json())
@@ -36,6 +39,18 @@ app.use(bp.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.render('index.ejs', { name: 'Joey'})
+})
+
+app.post('/logout', (req, res) => {
+    token = req.cookies.token
+    res.clearCookie(token);
+})
+
+app.get('/api/get/currentuser', function (req, res) {
+    token = req.cookies.token
+    const verified = jwt.verify(token, process.env.JWT_SECRET)
+    console.log(verified)
+    res.json(verified)
 })
 
 app.get('/login', (req, res) => {
@@ -46,8 +61,37 @@ app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/login', (req, res) => {
-    
+app.get('/home', (req, res) => {
+
+})
+
+app.post('/login', async(req, res) => {
+    const { name, password } = req.body
+	const user = await User.findOne({ name }).lean()
+
+	if (!user) {
+		return res.json({ status: 'error', error: 'Invalid username/password' })
+	}
+
+	if (await bcrypt.compare(password, user.password)) {
+		// the username, password combination is successful
+
+		const token = jwt.sign(
+			{
+				id: user._id,
+				name: user.name
+			},
+			JWT_SECRET, { expiresIn: "30days"}
+		)
+
+        res.cookie("token", token, {
+            httpOnly: true
+        })
+
+		return res.json({ status: 'ok', code: 200, data: token })
+	}
+
+	res.json({ status: 'error', code: 400, error: 'Invalid username/password' })
 })
 
 app.post('/register', async(req, res) => {
@@ -58,20 +102,25 @@ app.post('/register', async(req, res) => {
     const password = await bcrypt.hash(plainTextPassword, 10)
     console.log(password)
 
-    var user = new User({
-        name: name,
-        password: password
-    })
-
-    user.save(function(err, result) {
-        if (err) {
-            if (err.code == 11000) {
-                return res.json({ status: 'error', error: 'Username already in use'})
-            }
+    try {
+		const response = await User.create({
+            name: name,
+            password: password
+		})
+		console.log('User created successfully: ', response)
+	} catch (error) {
+		if (error.code === 11000) {
+			// duplicate key
+			return res.json({ status: 'error', code: 400, error: 'Username already in use' })
+		} else {
+            return res.json({ status: 'error', code:400, error: 'Unknown error code'})
         }
-        res.json({ status: 'ok'})
-        
-    })
+		throw error
+	}
+
+	res.json({ status: 'ok', code:200 })
+
+    
 })
 
 app.listen(process.env.PORT || 5000)
