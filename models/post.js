e5356615b6b5bec46cce2cd746905a26062844d1
mapleweekend <73198556+mapleweekend@users.noreply.github.com>
2021-11-12 22:08:40 -0500

    getting rid of comment

--- models/post.js
+++ models/post.js
@@ -19,7 +19,7 @@ const postSchema = new mongoose.Schema(
         current_user_upvoted: { type: Boolean},
         current_user_downvoted: { type: Boolean},
         current_user_admin: { type: Boolean},
-        comments: { type: Array}
+        comments: { type: Array},
     },
     { collection: 'posts'}
 )
