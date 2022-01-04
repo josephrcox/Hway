8b0b39f8f7435ffe2954690f97caa954771f1741
mapleweekend <joeyhobart@outlook.com>
2022-01-03 22:11:02 -0500

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -1362,7 +1362,7 @@ app.get('/api/get/search/', async (req, res) => {
     }
 });
 app.get('*', async (req, res) => {
-    res.render('error.ejs', { layout: 'layouts/error.ejs', topic: "PAGE NOT FOUND", error: ((req.url).replace('/', '').replaceAll('%20', ' ')) });
+    res.render('error.ejs', { layout: 'layouts/error.ejs', topic: "PAGE NOT FOUND", error: ((req.url).replace('/', '')) });
 });
 function getFullDateTimeAndTimeStamp() {
     let datetime = new Date();
