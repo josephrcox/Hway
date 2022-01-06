f8dba206d5dd28fbdd900979bf459f287ad4c9fa
mapleweekend <joeyhobart@outlook.com>
2022-01-05 19:16:17 -0500

    Fixed some directory stuff for typescript

--- src/server.ts
+++ src/server.ts
@@ -25,13 +25,13 @@ var postsPerPage = 30;
 let ms_in_day = 86400000
 
 app.set('view engine', 'ejs')
-app.set('views','./views')
+app.set('views',path.join(__dirname, './'))
 app.set('layout', 'layouts/layout')
 app.use(cors());
 app.use(express.json())
 app.use(expressLayouts)
-app.use(express.static(path.join(__dirname, './src/')));
-app.use(express.static('./src/'));
+app.use(express.static(path.join(__dirname, './dist/')));
+app.use(express.static('./dist/'));
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATEBASE_URL, {
