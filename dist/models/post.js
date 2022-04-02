7d7c8806e7c93819d2160ebe8fa96493e42f87b0
Joseph Cox <73198556+mapleweekend@users.noreply.github.com>
2022-04-02 15:27:38 -0400

    Update post.js

--- dist/models/post.js
+++ dist/models/post.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose')
 
-const commentSchema = new mongoose.Schema(
+const nestedCommentSchema = new mongoose.Schema(
     {
         body: { type:String, required:true },
-        nestedComments: { type:Array, required:false },
         poster: { type:String, required:true },
         posterID: { type:String, required:true },
         status: { type:String, required:true, default:"active "},
         total_votes: { type:Number, required:true, default:0 },
-        users_voted: { type:Array, required:true, default:[]}
+        users_voted: { type:Array, required:true, default:[]},
+        current_user_upvoted: { type: Boolean},
+        current_user_admin: { type: Boolean},
+    }, {timestamps:true}
+)
+
+const commentSchema = new mongoose.Schema(
+    {
+        body: { type:String, required:true },
+        nested_comments: [nestedCommentSchema],
+        poster: { type:String, required:true },
+        posterID: { type:String, required:true },
+        status: { type:String, required:true, default:"active "},
+        total_votes: { type:Number, required:true, default:0 },
+        users_voted: { type:Array, required:true, default:[]},
+        current_user_upvoted: { type: Boolean},
+        current_user_admin: { type: Boolean},
     }, {timestamps:true}
 )
 
